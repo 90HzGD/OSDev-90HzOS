2693279d36debeb06e9a6ddb7c399cdfc4997020
R4nd0mguy57 <iamtonpapa57@gmail.com>
2026-09-12 19:33:27 +0200

    VGA Cursor fix

--- 90HzOS/kernel/src/vga/VGAText.asm
+++ 90HzOS/kernel/src/vga/VGAText.asm
@@ -1,37 +1,38 @@
 [BITS 32]
 
-global update_cursor
+global update_cursor 
 extern outb
 
-update_cursor:
-    push ebx
+pos_low: db 0
+pos_high: db 0
 
-    mov ebx, [esp + 8] 
-
-    push dword 0x0E
-    push dword 0x3D4
-    call outb
-    add esp, 8
-
-    mov eax, ebx
-    shr eax, 8
-    and eax, 0xFF 
-    push eax
-    push dword 0x3D5
-    call outb
-    add esp, 8
-
-    push dword 0x0F
-    push dword 0x3D4
-    call outb
-    add esp, 8
-
-    mov eax, ebx
-    and eax, 0xFF
-    push eax
-    push dword 0x3D5
-    call outb
-    add esp, 8
+update_cursor: 
+    mov eax, [esp + 4] 
+    mov ecx, [esp + 8]
+    mov [pos_low], cl
+    mov [pos_high], al
+    mov eax, 0
+    mov ecx, 0
     
-    pop ebx
+    push dword 0x0E 
+    push dword 0x3D4 
+    call outb 
+    add esp, 8 
+    
+    mov ecx, [pos_low]
+    push ecx 
+    push dword 0x3D5 
+    call outb 
+    add esp, 8 
+    
+    push dword 0x0F 
+    push dword 0x3D4 
+    call outb 
+    add esp, 8
+
+    mov eax, [pos_high]
+    push eax 
+    push dword 0x3D5 
+    call outb 
+    add esp, 8 
     ret
